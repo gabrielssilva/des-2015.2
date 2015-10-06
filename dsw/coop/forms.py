19627223f6dd801f374d5301fc2814adeb1ed3c4
anacarolina1 <ana_karolopes@hotmail.com>
2015-10-06 18:39:58 -0300

    Tries to fix login

--- dsw/coop/forms.py
+++ dsw/coop/forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.forms import PasswordInput
 from django import forms
 from models import User
 
@@ -6,5 +7,12 @@ class UserForm(forms.ModelForm):
 
         class Meta:
                 model = User
-                fields = ['nome', 'email', 'telefone', 'senha']
-               
+                fields = ['nome', 'email', 'telefone']
+              	widgets = {
+            		'senha': PasswordInput()
+        		}
+
+class AuthenticationForm(forms.Form):
+
+    nome = forms.CharField(max_length=254)
+    senha = forms.CharField(widget=forms.PasswordInput)
--- dsw/coop/views.py
+++ dsw/coop/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
 from forms import UserForm
-from django.contrib.auth.forms import AuthenticationForm 
-from django.contrib.auth import login 
-
+from forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 from coop.models import User
 
@@ -14,20 +13,25 @@ from coop.models import User
 def home(request):
 	return render(request, "index.html")
 
+def homeLogged(request):
+    return render(request, "logged_index.html")
+
 def login(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST) # Veja a documentacao desta funcao
         
         if form.is_valid():
             #se o formulario for valido significa que o Django conseguiu encontrar o usuario no banco de dados
-            #agora, basta login o usuario e ser feliz.
-            login(request, form.get_user())
-            return HttpResponseRedirect("/") # redireciona o usuario logado para a pagina inicial
+            #agora, basta logar o usuario
+            login(request, form.get_model())
+            return HttpResponseRedirect("logged_index.html") # redireciona o usuario logado para a pagina inicial
         else:
             return render(request, "login.html", {"form": form})
     
     #se nenhuma informacao for passada, exibe a pagina de login com o formulario
+    
     return render(request, "login.html", {"form": AuthenticationForm()})
+
 
 #Page for register user
 class Register(CreateView):
