6e316d58ecefbda357f5b01ac10d3295b6e5e74b
Cristiano Costa <engcristianoc@gmail.com>
2015-10-06 12:19:01 -0300

    Testando classe User

--- dsw/coop/tests.py
+++ dsw/coop/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+class UserTest(TestCase):
+
+    def setUp(self):
+    	self.user = User(name="name", email="email", password="password")
+
+    	self.name = "name"
+    	self.email = "email"
+    	self.password = "password"
+
+    def testUnicode(self):
+   		self.assertEquals(self.name, unicode(self.user))
+   		self.assertEquals("email", self.email)
+   		self.assertEquals("password", self.password)
