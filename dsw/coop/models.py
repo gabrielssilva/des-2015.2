fdb14e96d24d405a0f2adf51f923e74c99f67f16
Cristiano Costa <engcristianoc@gmail.com>
2015-10-08 12:12:03 -0300

    fix conflicts

--- dsw/coop/models.py
+++ dsw/coop/models.py
@@ -32,15 +32,3 @@ class Player(AbstractBaseUser):
 		verbose_name = (u'name')
 		verbose_name_plural = (u'names')
 
-
-class Game(models.Model):
-	class Admin:
-		pass
-	def __str__(self):
-		return self.name
-	player_id = models.ForeignKey(Player)
-	name = models.CharField(max_length=100)
-	console = models.CharField(max_length=20)
-	gender = models.CharField(max_length=10)
-	language = models.CharField(max_length=10)
-	desc_state = models.TextField()
--- dsw/game/models.py
+++ dsw/game/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from coop.models import User
-
+from coop.models import Player
 class Game(models.Model):
 	class Admin:
 		pass
 	def __str__(self):
 		return self.name
-	user_id = models.ForeignKey(User)
+	user_id = models.ForeignKey(Player)
 	name = models.CharField(max_length=100)
 	console = models.CharField(max_length=20)
 	gender = models.CharField(max_length=10)
