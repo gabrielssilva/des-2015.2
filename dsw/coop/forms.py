7879a5354a5e7ba28946d91e3f054f2422702b5a
anacarolina1 <ana_karolopes@hotmail.com>
2015-11-21 19:44:00 -0200

    Add search box

--- dsw/coop/forms.py
+++ dsw/coop/forms.py
@@ -7,7 +7,13 @@ class PlayerForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = Player
-        fields = ['email', 'password', 'nome', 'telefone']
+        fields = ['email', 'nome', 'telefone', 'password']
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
 class AuthenticationForm(forms.Form):
     email = forms.CharField(max_length=254)
